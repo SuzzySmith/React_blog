fe26f9e85f5e11dda38aabb79b0f256e37228273
Suzzy Smith <64595087+SuzzySmith@users.noreply.github.com>
2020-09-09 18:46:14 +0000

    Add files via upload

---
+++ src/components/SinglePost.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+ //import Divider from '@material-ui/core/Divider';
+import renderHTML from 'react-render-html';
+ 
+class SinglePost extends Component {
+ 
+    showPost = (props) => {
+        if (!props.post) return null;
+ 
+        const {title, author, body, datestamp} = this.props.post;
+ 
+        return (
+            <React.Fragment>
+ 
+                <div className="single_post"> 
+                    <h4>Title: {title}</h4>
+        
+                    <p><b>Autor:</b> {author}</p>
+    
+                    <p><b>Content:</b> {body}</p>
+                     
+                    <div style={{ width: '60%' }}>{renderHTML(body)}</div>
+                </div>
+            </React.Fragment>
+        )
+ 
+    }
+    render() {
+        return (
+            <div className=" col-md-6">
+                {this.showPost(this.props)} 
+            </div>
+        );
+    }
+}
+ 
+ 
+export default SinglePost;
